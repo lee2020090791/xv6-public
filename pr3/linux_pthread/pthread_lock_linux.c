bd17fb3b744f2a5eda12ea3c286f93ceac3f93e0
lee2020090791 <ljs3463@hanyang.ac.kr>
2024-05-18 16:44:29 +0900

    lock

--- pr3/linux_pthread/pthread_lock_linux.c
+++ pr3/linux_pthread/pthread_lock_linux.c
@@ -1,53 +1,68 @@
 #include <stdio.h>
 #include <pthread.h>
-typedef unsigned int uint;
-struct mutex{
-    uint locked;
-    char* name;
-};
-
-int shared_resource = 0;
 
 #define NUM_ITERS 10
 #define NUM_THREADS 10
 
-int n;
+int shared_resource = 0;
+int turn=0;
+int flag[NUM_THREADS];
+int n=0;
+
+
 
 void lock();
 void unlock();
 
-void lock()
+void lock(int tid)
 {
-
+    int j;
+    do{ // flag 0 for lv1, flag 1 for finish cs, flag 2 for ready to enter cs(lv2)
+        // lv1
+        flag[tid]=0; 
+        while(turn!=tid){
+            if(flag[turn]==1){
+                turn = tid;
+            }
+        }
+        //lv2
+        flag[tid]=2;
+        j=0;
+        while((j<n)&&(j==tid||flag[j]!=2)){ // check other thread is in lv2
+            j=j+1;
+        }
+    } while(j<n);    
 }
 
-void unlock()
+void unlock(int tid)
 {
+    flag[tid]=1;
 }
 
 void* thread_func(void* arg) {
+    n++;
     int tid = *(int*)arg;
+    lock(tid);
     
-    lock();
+    for(int i = 0; i < NUM_ITERS; i++)    shared_resource++;
     
-        for(int i = 0; i < NUM_ITERS; i++)    shared_resource++;
-    
-    unlock();
+    unlock(tid);
     
     pthread_exit(NULL);
 }
 
 int main() {
-    pthread_t threads[n];
-    int tids[n];
+    pthread_t threads[NUM_THREADS];
+    int tids[NUM_THREADS];
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < NUM_THREADS; i++) {
         tids[i] = i;
         pthread_create(&threads[i], NULL, thread_func, &tids[i]);
     }
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
+        n--;
     }
 
     printf("shared: %d\n", shared_resource);
